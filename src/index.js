f20f8f2e397a88dea4189281050bc0f9a2536bb7
shella12 <ayeshaarshad4567@gmail.com>
2022-09-07 21:03:06 +0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import './style.css';
-import pokemons from './pokemons';
-import  likesPost,{likeGet} from './likes.js';
+import pokemons from './pokemons.js';
+import likesPost, { likeGet } from './likes.js';
 
-const itemNumbers=document.getElementById('udateItemNumbers');
-itemNumbers.innerHTML='Pokemons(20)';
+const itemNumbers = document.getElementById('udateItemNumbers');
+itemNumbers.innerHTML = 'Pokemons(20)';
 likeGet('0YSAdjw9FalqCv0vGDYS')
-.then(data =>{
-    data.forEach(element => {
-     const targetItem=element[1].item_id.split('likeId').slice(1);
-     document.querySelector(`#like-count${targetItem[0]}`).innerHTML = `${element[1].likes} Likes`;
- });
- });
-document.body.addEventListener('click',(e)=>{
-if (e.target && e.target.className==='likeBtn'){
-    likesPost('0YSAdjw9FalqCv0vGDYS',e.target);
+  .then((data) => {
+    data.forEach((element) => {
+      const targetItem = element[1].item_id.split('likeId').slice(1);
+      document.querySelector(`#like-count${targetItem[0]}`).innerHTML = `${element[1].likes} Likes`;
+    });
+  });
+document.body.addEventListener('click', (e) => {
+  if (e.target && e.target.className === 'likeBtn') {
+    likesPost('0YSAdjw9FalqCv0vGDYS', e.target);
     likeGet('0YSAdjw9FalqCv0vGDYS')
-    .then(data =>{
-       data.forEach(element => {
-        const targetItem=element[1].item_id.split('likeId').slice(1);
-        document.querySelector(`#like-count${targetItem[0]}`).innerHTML = `${element[1].likes} Likes`;
-    });
-    });
-}
+      .then((data) => {
+        data.forEach((element) => {
+          const targetItem = element[1].item_id.split('likeId').slice(1);
+          document.querySelector(`#like-count${targetItem[0]}`).innerHTML = `${element[1].likes} Likes`;
+        });
+      });
+  }
 });
 pokemons();
--- src/pokemonDetail.js
+++ src/pokemonDetail.js
@@ -1,33 +1,31 @@
 import cancelBtnImage from './cancel.png';
 
-const cards=document.body.querySelector('.cards');
-const popup=document.createElement('div');
-popup.className='popup'
+const cards = document.body.querySelector('.cards');
+const popup = document.createElement('div');
+popup.className = 'popup';
 
-cards.addEventListener('click',(e)=>{
-    if(e.target && e.target.id== 'cancelBtn'){
-        popup.style.display='none';
-        document.body.style.overflowY='scroll';
-     }
- });
-const detail = (url)=>{
-fetch(url).then(response => response.json())
-.then(data =>{
-    let pokeName=data.name;
-        pokeName= pokeName[0].toUpperCase() + pokeName.slice(1);
-     let typeOfPoke='',i=1;
-        const n=data.types.length;
-        data.types.forEach(element => {
-        if(n===i)
-        typeOfPoke += element.type.name;
-        else
-        typeOfPoke += element.type.name + ', ';
-        i++;
-    });
+cards.addEventListener('click', (e) => {
+  if (e.target && e.target.id === 'cancelBtn') {
+    popup.style.display = 'none';
+    document.body.style.overflowY = 'scroll';
+  }
+});
+const detail = (url) => {
+  fetch(url).then((response) => response.json())
+    .then((data) => {
+      let pokeName = data.name;
+      pokeName = pokeName[0].toUpperCase() + pokeName.slice(1);
+      let typeOfPoke = ''; let i = 1;
+      const n = data.types.length;
+      data.types.forEach((element) => {
+        if (n === i) typeOfPoke += element.type.name;
+        else typeOfPoke += `${element.type.name}, `;
+        i += 1;
+      });
 
-    const popupCode=` 
+      const popupCode = ` 
     <img id="cancelBtn" src=${cancelBtnImage} alt="cancel popup image">
-    <img id="pokemonImage" src="${data.sprites.other["official-artwork"].front_default}" alt="Pokemon image">
+    <img id="pokemonImage" src="${data.sprites.other['official-artwork'].front_default}" alt="Pokemon image">
     <h1>${pokeName}</h1>
     <div class="power">
         <p>HP: ${data.base_experience}</p>
@@ -38,14 +36,14 @@ fetch(url).then(response => response.json())
         <p>Height: ${data.height}</p>
     </div>
     <h2>Comments(2)</h2>
-`
-    popup.innerHTML = popupCode;
-    popup.style.display='flex';
-    cards.appendChild(popup);
-    document.body.style.overflowY='hidden';
-})
-.catch(err => {
-    console.log("Couldn't fetch Pokemons details ",err)
-})
-}
+`;
+      popup.innerHTML = popupCode;
+      popup.style.display = 'flex';
+      cards.appendChild(popup);
+      document.body.style.overflowY = 'hidden';
+    })
+    .catch((err) => {
+      console.log("Couldn't fetch Pokemons details ", err);
+    });
+};
 export default detail;
