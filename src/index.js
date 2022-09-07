728e8752300613fcad15574204ded8ed778dd04a
Newton <altontonnalumasa@gmail.com>
2022-09-07 16:23:59 +0300

    add comment api

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+import addComment1 from './module/addComment.js';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newid = document.querySelector('.comments').getAttribute('data-id');
+  const names = document.querySelector('.name').value;
+  const insights = document.querySelector('.insights').value;
+  const commmentObj = {
+    item_id: newid,
+    username: names,
+    comment: insights,
+  };
+  addComment1(commmentObj, newid);
+});
