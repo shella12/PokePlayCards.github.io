db78324fb8b8e866c144169b32f42992d2ae946e
shella12 <ayeshaarshad4567@gmail.com>
2022-09-08 21:23:20 +0500

    Add comment button work

--- src/pokemons.js
+++ src/pokemons.js
@@ -4,7 +4,7 @@ import detail from './pokemonDetail.js';
 let numberOfItems = 0;
 const cards = document.body.querySelector('.cards');
 cards.addEventListener('click', (e) => {
-  if (e.target && e.target.className === 'pokemon-img') {
+  if (e.target && e.target.className === 'comment') {
     const url = `https://pokeapi.co/api/v2/pokemon/${e.target.id}`;
     detail(url,e.target.id);
   }
@@ -23,14 +23,14 @@ const pokemons = async (url) => {
           .then((data) => data.sprites.other['official-artwork'].front_default)
           .then((src) => {
             const card = `<div class="card">
-            <img class="pokemon-img" id=${id + 1} src="${src}" alt="Pokemon1">
+            <img class="pokemon-img" src="${src}" alt="Pokemon1">
             <div class="card-title">
             <h2>${pokemonName}</h2>
             <img id="likeId${id + 1}" class="likeBtn" src=${unlike} alt="heart icon for like">
             </div> 
             <p class="like-count" id="like-count${id + 1}">0 Likes</p>
             <div class="buttons">
-                <button type="button" class="comment">Comments</button>
+                <button type="button" id=${id + 1} class="comment">Comments</button>
                 <button type="button" class="reservations">Reservations</button>
             </div>
             
